3915841a5929454d2ef6f897e96f673f39a9c0a9
MarkShark99 <markdw88@gmail.com>
2018-02-09 14:06:19 -0500

    Initial commit.

---
+++ src/org/usfirst/frc/team4237/robot/Autonomous.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4237.robot;
+
+import org.usfirst.frc.team4237.robot.components.Drivetrain;
+import org.usfirst.frc.team4237.robot.components.Elevator;
+import org.usfirst.frc.team4237.robot.components.Gripper;
+import org.usfirst.frc.team4237.robot.network.RaspberryPiReceiver;
+
+public class Autonomous
+{
+	private Drivetrain drivetrain = Drivetrain.getInstance();
+	private Elevator elevator = Elevator.getInstance();
+	private Gripper gripper = Gripper.getInstance();
+	private RaspberryPiReceiver raspberryPiReceiver = RaspberryPiReceiver.getInstance();
+	
+	private static Autonomous instance = new Autonomous();
+	public static Autonomous getInstance()
+	{
+		return instance;
+	}
+	
+	private Autonomous()
+	{
+		raspberryPiReceiver.start();
+	}
+	
+	/**
+	 * Autonomous init method
+	 */
+	public void init()
+	{
+		
+	}
+	
+	/**
+	 * Autonomous loop
+	 */
+	public void periodic()
+	{
+		
+	}
+}
