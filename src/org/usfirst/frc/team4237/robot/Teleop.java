c079d1004a5ebab2e93545cace519d406c83f871
MarkShark99 <markdw88@gmail.com>
2018-02-09 15:18:36 -0500

    Changed to use DriverXbox class

--- src/org/usfirst/frc/team4237/robot/Teleop.java
+++ src/org/usfirst/frc/team4237/robot/Teleop.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team4237.robot;
 import org.usfirst.frc.team4237.robot.components.Drivetrain;
 import org.usfirst.frc.team4237.robot.components.Elevator;
 import org.usfirst.frc.team4237.robot.components.Gripper;
+import org.usfirst.frc.team4237.robot.control.DriverXbox;
 import org.usfirst.frc.team4237.robot.control.Xbox;
 
 public class Teleop
@@ -10,7 +11,7 @@ public class Teleop
 	private Drivetrain drivetrain = Drivetrain.getInstance();
 	private Elevator elevator = Elevator.getInstance();
 	private Gripper gripper = Gripper.getInstance();
-	private Xbox xbox = Xbox.getInstance();
+	private DriverXbox xbox = DriverXbox.getInstance();
 	
 	private boolean aButton = false;
 	private boolean bButton = false;
