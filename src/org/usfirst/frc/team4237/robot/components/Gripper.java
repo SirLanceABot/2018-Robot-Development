01c1e5f1918927c1af9f2baf1fa1367f08d73fa8
MarkShark99 <markdw88@gmail.com>
2018-02-09 14:05:39 -0500

    Added many variables, quarantined others, and rewrote a lot of code.

--- src/org/usfirst/frc/team4237/robot/components/Gripper.java
+++ src/org/usfirst/frc/team4237/robot/components/Gripper.java
@@ -1,11 +1,11 @@
 package org.usfirst.frc.team4237.robot.components;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import org.usfirst.frc.team4237.robot.control.Xbox;
-
-import edu.wpi.first.wpilibj.Timer;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.usfirst.frc.team4237.robot.components.Gripper.Constants.Range;
+import org.usfirst.frc.team4237.robot.control.Xbox;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Gripper class
@@ -14,63 +14,73 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Gripper extends Thread
 {
 	private Xbox xbox = Xbox.getInstance();
-	private static Gripper instance = new Gripper();
 
+	private static Gripper instance = new Gripper();
 	public static Gripper getInstance()
 	{
 		return instance;
 	}
+
+	private WPI_TalonSRX leftIntakeTalon = new WPI_TalonSRX(Constants.LEFT_INTAKE_MOTOR_PORT);
+	private WPI_TalonSRX rightIntakeTalon = new WPI_TalonSRX(Constants.RIGHT_INTAKE_MOTOR_PORT);
+	private WPI_TalonSRX pivoter = new WPI_TalonSRX(Constants.pivoter_MOTOR_PORT);
+
+	private boolean isAutoEjecting = false;
+	private boolean isAutoIntaking = false;
+	private boolean isPivoting = false;
 	
-	WPI_TalonSRX leftIntake = new WPI_TalonSRX(Constants.LEFT_INTAKE_MOTOR_PORT);
-	WPI_TalonSRX rightIntake = new WPI_TalonSRX(Constants.RIGHT_INTAKE_MOTOR_PORT);
-	WPI_TalonSRX pivot = new WPI_TalonSRX(Constants.PIVOT_MOTOR_PORT);
+	private int currentPivotValue = 0;
+	private Constants.Range currentRange;
 	
-	boolean isIntakeDone = true;
-	boolean isPivotRaisedDone = true;
-	boolean isPivotMiddleDone = true;
-	boolean isPivotFloorDone = true;
-	
-	boolean isAutoPivotRaisedDone = false;
-	boolean isAutoPivotMiddleDone = false;
-	boolean isAutoPivotFloorDone = false;
-	
+	//Quarantine
+	private boolean isIntakeDone = true;
+	boolean ispivoterRaisedDone = true;
+	boolean ispivoterMiddleDone = true;
+	boolean ispivoterFloorDone = true;
+
+	boolean isAutopivoterRaisedDone = false;
+	boolean isAutopivoterMiddleDone = false;
+	boolean isAutopivoterFloorDone = false;
+
 	private Constants.Direction pivotDirection = Constants.Direction.None;
-	
+
 	public Gripper()
 	{
-		//TODO: Disable Soft lImite switches
-		leftIntake.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
-		leftIntake.setSensorPhase(false);
-		rightIntake.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
-		rightIntake.setSensorPhase(true);
-		
-		leftIntake.configForwardSoftLimitEnable(false, 0);
-		leftIntake.configReverseSoftLimitEnable(false, 0);
-		rightIntake.configForwardSoftLimitEnable(false, 0);
-		rightIntake.configReverseSoftLimitEnable(false, 0);
-		
-		rightIntake.setInverted(true);
-		
-		pivot.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
-		pivot.setSensorPhase(false);
-		pivot.configForwardSoftLimitEnable(false, 0);
-		pivot.configReverseSoftLimitEnable(false, 0);
-		
-		leftIntake.selectProfileSlot(Constants.PID_SLOT_ID, 0);
-		leftIntake.config_kP(Constants.PID_SLOT_ID, 2.2, 1);
-		leftIntake.config_kI(Constants.PID_SLOT_ID, 0.01, 1);
-		leftIntake.config_kD(Constants.PID_SLOT_ID, 0.0001, 1);
-		leftIntake.config_kF(Constants.PID_SLOT_ID, 4.3, 1);
-		
-		rightIntake.selectProfileSlot(Constants.PID_SLOT_ID, 0);
-		rightIntake.config_kP(Constants.PID_SLOT_ID, 2.2, 1);
-		rightIntake.config_kI(Constants.PID_SLOT_ID, 0.01, 1);
-		rightIntake.config_kD(Constants.PID_SLOT_ID, 0.0001, 1);
-		rightIntake.config_kF(Constants.PID_SLOT_ID, 4.3, 1);
+		//Left & right intake talon settings
+		leftIntakeTalon.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
+		leftIntakeTalon.setSensorPhase(false);
+		leftIntakeTalon.configForwardSoftLimitEnable(false, 0);
+		leftIntakeTalon.configReverseSoftLimitEnable(false, 0);
+		leftIntakeTalon.setInverted(false);
+
+		rightIntakeTalon.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
+		rightIntakeTalon.setSensorPhase(true);
+		rightIntakeTalon.configForwardSoftLimitEnable(false, 0);
+		rightIntakeTalon.configReverseSoftLimitEnable(false, 0);
+		rightIntakeTalon.setInverted(true);
+
+		//Pivoter settings
+		pivoter.configSelectedFeedbackSensor(com.ctre.phoenix.motorcontrol.FeedbackDevice.QuadEncoder, 0, 0);
+		pivoter.setSensorPhase(false);
+		pivoter.configForwardSoftLimitEnable(false, 0);
+		pivoter.configReverseSoftLimitEnable(false, 0);
+
+		//PID settings
+		leftIntakeTalon.selectProfileSlot(Constants.PID_SLOT_ID, 0);
+		leftIntakeTalon.config_kP(Constants.PID_SLOT_ID, 2.2, 1);
+		leftIntakeTalon.config_kI(Constants.PID_SLOT_ID, 0.01, 1);
+		leftIntakeTalon.config_kD(Constants.PID_SLOT_ID, 0.0001, 1);
+		leftIntakeTalon.config_kF(Constants.PID_SLOT_ID, 4.3, 1);
+
+		rightIntakeTalon.selectProfileSlot(Constants.PID_SLOT_ID, 0);
+		rightIntakeTalon.config_kP(Constants.PID_SLOT_ID, 2.2, 1);
+		rightIntakeTalon.config_kI(Constants.PID_SLOT_ID, 0.01, 1);
+		rightIntakeTalon.config_kD(Constants.PID_SLOT_ID, 0.0001, 1);
+		rightIntakeTalon.config_kF(Constants.PID_SLOT_ID, 4.3, 1);
 	}
 	public void resetEncoders()
 	{
-		pivot.setSelectedSensorPosition(0,0,0);
+		pivoter.setSelectedSensorPosition(0,0,0);
 	}
 
 	/**
@@ -79,55 +89,59 @@ public class Gripper extends Thread
 	 */
 	public boolean autoIntake()
 	{
-		if(isIntakeDone == true)
+		if(!isAutoIntaking())
 		{
-			isIntakeDone = false;
-			leftIntake.setSelectedSensorPosition(0,0,0);
-			rightIntake.setSelectedSensorPosition(0,0,0);		// set encoder position
-			leftIntake.set(ControlMode.Velocity, 1);
-			rightIntake.set(ControlMode.Velocity, 1);		// set motor
+			setAutoIntaking(true);
+			leftIntakeTalon.setSelectedSensorPosition(0,0,0);
+			rightIntakeTalon.setSelectedSensorPosition(0,0,0);		// set encoder position
+
+			leftIntakeTalon.set(ControlMode.Velocity, 1);
+			rightIntakeTalon.set(ControlMode.Velocity, 1);		// set motor
 		}
-		else if(rightIntake.getSelectedSensorPosition(0) >= Constants.SUCK_CUBE_IN)
+		if(rightIntakeTalon.getSelectedSensorPosition(0) >= Constants.AUTO_INTAKE_ENCODER_STOP_VALUE)
 		{
-			intakeOff();
-			isIntakeDone = true;
+			disable();
+			setAutoIntaking(false);
 		}
-		
+
 		return isIntakeDone;
 	}
 
 	/** 
-	 * eject function for autonomous
+	 * Eject function for autonomous
 	 * @return State of eject, whether it's done or not
 	 */
 	public boolean autoEject()
 	{
-		if(isIntakeDone == true)
+		if(isAutoEjecting())
 		{
-			isIntakeDone = false;
-			rightIntake.setSelectedSensorPosition(0,0,0);		// set encoder position
-			leftIntake.set(ControlMode.Velocity, -1);
-			rightIntake.set(ControlMode.Velocity, -1);
+			setAutoEjecting(true);
+
+			rightIntakeTalon.setSelectedSensorPosition(0, 0, 0);		// set encoder position
+			leftIntakeTalon.set(ControlMode.Velocity, -1);
+			rightIntakeTalon.set(ControlMode.Velocity, -1);
 		}
-		else if(rightIntake.getSelectedSensorPosition(0) >= Constants.SPIT_CUBE_OUT)
+		//TODO: Check logic, should it be <= or should it be >= like it was originally?
+		if(rightIntakeTalon.getSelectedSensorPosition(0) <= Constants.AUTO_EJECT_ENCODER_STOP_VALUE)
 		{
-			intakeOff();
-			isIntakeDone = true;
+			disable();
+			setAutoEjecting(false);
 		}
-		
+
 		return isIntakeDone;
 	}
 
 	/**
 	 * Turn off intake
 	 */
-	public void intakeOff()
+	public void disable()
 	{
-		//leftIntake.set(ControlMode.Velocity, 0);
-		//rightIntake.set(ControlMode.Velocity, 0);
-		
-		leftIntake.set(ControlMode.PercentOutput, 0);
-		rightIntake.set(ControlMode.PercentOutput, 0);
+		//TODO: Make velocity-based control work
+		//leftIntakeTalon.set(ControlMode.Velocity, 0);
+		//rightIntakeTalon.set(ControlMode.Velocity, 0);
+
+		leftIntakeTalon.set(ControlMode.PercentOutput, 0);
+		rightIntakeTalon.set(ControlMode.PercentOutput, 0);
 	}
 
 	/**
@@ -135,195 +149,191 @@ public class Gripper extends Thread
 	 */
 	public void intake()
 	{
-		leftIntake.set(ControlMode.PercentOutput, -0.5);
-		rightIntake.set(ControlMode.PercentOutput, -0.5);
-		//leftIntake.set(ControlMode.Velocity, -0.5);
-		//rightIntake.set(ControlMode.Velocity, -0.5);
+		//TODO: Make velocity-based control work
+		//leftIntakeTalon.set(ControlMode.Velocity, -0.5);
+		//rightIntakeTalon.set(ControlMode.Velocity, -0.5);
+
+		leftIntakeTalon.set(ControlMode.PercentOutput, -0.5);
+		rightIntakeTalon.set(ControlMode.PercentOutput, -0.5);
 	}
-	
+
 	/**
 	 * Eject for use during tele-op
 	 */
 	public void eject()
 	{
-		//leftIntake.set(ControlMode.Velocity, 500);
-		//rightIntake.set(ControlMode.Velocity, 500);
-		
-		leftIntake.set(ControlMode.PercentOutput, 0.5);
-		rightIntake.set(ControlMode.PercentOutput, 0.5);
+		//leftIntakeTalon.set(ControlMode.Velocity, 500);
+		//rightIntakeTalon.set(ControlMode.Velocity, 500);
+
+		leftIntakeTalon.set(ControlMode.PercentOutput, 0.5);
+		rightIntakeTalon.set(ControlMode.PercentOutput, 0.5);
 	}
-	
+
 	public void spinLeft()
 	{
-		leftIntake.set(ControlMode.PercentOutput, -0.3);
-		rightIntake.set(ControlMode.PercentOutput, 0.3);
+		leftIntakeTalon.set(ControlMode.PercentOutput, -0.3);
+		rightIntakeTalon.set(ControlMode.PercentOutput, 0.3);
 	}
-	
+
 	public void spinRight()
 	{
-		leftIntake.set(ControlMode.PercentOutput, 0.3);
-		rightIntake.set(ControlMode.PercentOutput, -0.3);
+		leftIntakeTalon.set(ControlMode.PercentOutput, 0.3);
+		rightIntakeTalon.set(ControlMode.PercentOutput, -0.3);
 	}
-	
+
 	/**
-	 *  Angle gripper
-	 * @param value from joystick
+	 * Move gripper at selected speed
+	 * @param value Value from joystick
 	 */
 	public void pivot(double value)
 	{
-		pivot.set(value);
+		pivoter.set(value);
 	}
 
 	/**
 	 * Stop pivoting
 	 */
-	public void pivotOff()
+	public void pivoterOff()
 	{
-		pivot.set(0.0);
+		pivoter.set(0.0);
 	}
-	
+
 	@Override
 	public synchronized void run()
 	{
 		while (!this.interrupted())
 		{
-			boolean aButton = xbox.getRawButton(1);
-			boolean xButton = xbox.getRawButton(3);
-			boolean yButton = xbox.getRawButton(4);
+			boolean aButton = xbox.getRawButton(Xbox.Constants.A_BUTTON);
+			boolean bButton = xbox.getRawButton(Xbox.Constants.B_BUTTON);
+			boolean yButton = xbox.getRawButton(Xbox.Constants.Y_BUTTON);
+
+			updateCurrentRange();
 			
+			if (!isPivoting())
+			{
+				
+			}
 			
-			if(yButton || !isPivotRaisedDone)
+			if(yButton || !ispivoterRaisedDone)
 			{	
-				isPivotRaisedDone = false;
-				isPivotMiddleDone = true;
-				isPivotFloorDone = true;
-				if(pivot.getSelectedSensorPosition(0) < Constants.ENCODER_RAISED_POSITION)
+				ispivoterRaisedDone = false;
+				ispivoterMiddleDone = true;
+				ispivoterFloorDone = true;
+				if(pivoter.getSelectedSensorPosition(0) < Constants.ENCODER_RAISED_POSITION)
 				{
 					pivot(0.5);
 				}
 				else
 				{
-					isPivotRaisedDone = true;
-					pivotOff();
+					ispivoterRaisedDone = true;
+					pivoterOff();
 				}
-
-				
 			}
-			if(xButton || !isPivotMiddleDone)
+			if(bButton || !ispivoterMiddleDone)
 			{
-				isPivotFloorDone = true;
-				isPivotRaisedDone = true;
-				
-				if(isPivotMiddleDone)
+				ispivoterFloorDone = true;
+				ispivoterRaisedDone = true;
+
+				if(ispivoterMiddleDone)
 				{
-					isPivotMiddleDone = false;
-					if(pivot.getSelectedSensorPosition(0) > Constants.ENCODER_MIDDLE_POSITION)	// If it is above the switch
+					ispivoterMiddleDone = false;
+					if(pivoter.getSelectedSensorPosition(0) > Constants.ENCODER_MIDDLE_POSITION)	// If it is above the switch
 					{
 						pivot(-0.5);
 						pivotDirection = Constants.Direction.Down;
 					}
-					else if(pivot.getSelectedSensorPosition(0) < Constants.ENCODER_MIDDLE_POSITION)	// If it is below the switch
+					else if(pivoter.getSelectedSensorPosition(0) < Constants.ENCODER_MIDDLE_POSITION)	// If it is below the switch
 					{
 						pivot(0.5);  
 						pivotDirection = Constants.Direction.Up;
 					}
 				}
-				else if(((pivot.getSelectedSensorPosition(0) >= Constants.ENCODER_MIDDLE_POSITION) && (pivotDirection == Constants.Direction.Up)) || ((pivot.getSelectedSensorPosition(0) <= Constants.ENCODER_MIDDLE_POSITION) && (pivotDirection == Constants.Direction.Down)))
+				else if(((pivoter.getSelectedSensorPosition(0) >= Constants.ENCODER_MIDDLE_POSITION) && (pivotDirection == Constants.Direction.Up)) || ((pivoter.getSelectedSensorPosition(0) <= Constants.ENCODER_MIDDLE_POSITION) && (pivotDirection == Constants.Direction.Down)))
 				{
-					isPivotMiddleDone = true;
-					pivotOff();
+					ispivoterMiddleDone = true;
+					pivoterOff();
 				}
 
 			}
-			if(aButton || !isPivotFloorDone)
+			if(aButton || !ispivoterFloorDone)
 			{
-				isPivotFloorDone = false;
-				isPivotMiddleDone = true;
-				isPivotRaisedDone = true;
-				
-				if(pivot.getSelectedSensorPosition(0) > Constants.ENCODER_FLOOR_POSITION)
+				ispivoterFloorDone = false;
+				ispivoterMiddleDone = true;
+				ispivoterRaisedDone = true;
+
+				if(pivoter.getSelectedSensorPosition(0) > Constants.ENCODER_FLOOR_POSITION)
 				{
 					pivot(-0.5);
 				}
 				else
 				{
-					pivotOff();
-					isPivotFloorDone = true;
+					pivoterOff();
+					ispivoterFloorDone = true;
 				}
 			}
-			
+
 			Timer.delay(0.005);
 		}
 	}
+	
+	public void updateCurrentRange()
+	{
+		currentPivotValue = (int)getPivoterEncoder();
+		if (currentPivotValue <= Constants.Range.floorRange.topValue())
+		{
 			
+		}
+		else if (currentPivotValue <= Constants.Range.floorMiddleRange.topValue())
+		{
+			
+		}
+		else if (currentPivotValue <= Constants.Range.middleRange.topValue())
+		{
+			
+		}
+		else if (currentPivotValue <= Constants.Range.middleRaisedRange.topValue())
+		{
+			
+		}
+		else if (currentPivotValue <= Constants.Range.raisedRange.topValue())
+		{
+			
+		}
+	}
 
-	public boolean autoPivotUp()
-	{
-		isAutoPivotRaisedDone = false;
-		
-		if(pivot.getSelectedSensorPosition(0) < Constants.ENCODER_RAISED_POSITION)
-		{
-			pivot(0.5);
-		}
-		else
-		{
-			pivotOff();
-			isAutoPivotRaisedDone = true;
-		}
-		return isAutoPivotRaisedDone;
-	}
-	
-	public boolean autoPivotFloor()
-	{	
-		if(pivot.getSelectedSensorPosition(0) > Constants.ENCODER_FLOOR_POSITION && (isAutoPivotMiddleDone && isAutoPivotRaisedDone))
-		{
-			isAutoPivotFloorDone = false;
-			pivot(-0.5);
-		}
-		else
-		{
-			pivotOff();
-			isAutoPivotFloorDone = true;
-		}
-		return isAutoPivotFloorDone;
-	}
-	
-	/**
-	 * Pivot to angle of switch
-	 * @return State of pivot, whether it's done or not
-	 */
-	public boolean autoPivotMiddle()
-	{
-		if(isAutoPivotMiddleDone)
-		{
-			isAutoPivotMiddleDone = false;
-			if(pivot.getSelectedSensorPosition(0) > Constants.ENCODER_MIDDLE_POSITION)	// If it is above the switch
-			{
-				pivot(-0.5);
-				pivotDirection = Constants.Direction.Down;
-			}
-			else if(pivot.getSelectedSensorPosition(0) < Constants.ENCODER_MIDDLE_POSITION)	// If it is below the switch
-			{
-				pivot(0.5);  
-				pivotDirection = Constants.Direction.Up;
-			}
-		}
-		else if(((pivot.getSelectedSensorPosition(0) >= Constants.ENCODER_MIDDLE_POSITION) && (pivotDirection == Constants.Direction.Up)) || ((pivot.getSelectedSensorPosition(0) <= Constants.ENCODER_MIDDLE_POSITION) && (pivotDirection == Constants.Direction.Down)))
-		{
-			pivotOff();
-			isAutoPivotMiddleDone = true;
-		}
-		return isAutoPivotMiddleDone;
-	}
-	
-	
 	public int[] getVelocityArray()
 	{
-		return new int[] {leftIntake.getSelectedSensorVelocity(0), rightIntake.getSelectedSensorVelocity(0)};
+		return new int[] {leftIntakeTalon.getSelectedSensorVelocity(0), rightIntakeTalon.getSelectedSensorVelocity(0)};
 	}
-	public double getPivotEncoder()
+	
+	public double getPivoterEncoder()
 	{
-		return pivot.getSelectedSensorPosition(0);
+		return pivoter.getSelectedSensorPosition(0);
+	}
+
+	public boolean isAutoEjecting()
+	{
+		return isAutoEjecting;
+	}
+
+	public void setAutoEjecting(boolean isAutoEjecting)
+	{
+		this.isAutoEjecting = isAutoEjecting;
+	}
+
+	public boolean isAutoIntaking()
+	{
+		return isAutoIntaking;
+	}
+
+	public void setAutoIntaking(boolean isAutoIntaking)
+	{
+		this.isAutoIntaking = isAutoIntaking;
+	}
+	
+	public boolean isPivoting()
+	{
+		return isPivoting;
 	}
 
 	/**
@@ -338,18 +348,97 @@ public class Gripper extends Thread
 			None
 		}
 		
+		private enum InitRange
+		{
+			floorRange(0, 20),
+			floorMiddleRange(20, 2090),
+			middleRange(2090, 3010),
+			middleRaisedRange(3010, 3980),
+			raisedRange(5980, 6000),
+			none(-1, -1),
+			error(-1, -1);
+
+			private final double[] range;
+
+			InitRange(double bottomValue, double topValue)
+			{
+				this.range = new double[] {bottomValue, topValue};
+			}
+
+			public double[] range()
+			{
+				return this.range;
+			}
+
+			public double bottomValue()
+			{
+				return this.range[0];
+			}
+
+			public double topValue()
+			{
+				return this.range[1];
+			}
+		}
+
+		public enum Range
+		{
+			floorRange(        InitRange.floorRange.range(),        InitRange.floorRange,  InitRange.middleRange),
+			floorMiddleRange(  InitRange.floorMiddleRange.range(),  InitRange.floorRange,  InitRange.middleRange),
+			middleRange(       InitRange.middleRange.range(),       InitRange.floorRange,  InitRange.raisedRange),
+			middleRaisedRange( InitRange.middleRaisedRange.range(), InitRange.middleRange, InitRange.raisedRange),
+			raisedRange(       InitRange.raisedRange.range(),       InitRange.middleRange, InitRange.raisedRange),
+			error(             InitRange.error.range(),             InitRange.error,       InitRange.error);
+
+			private final double[] range;
+			private final InitRange higherNeighbor;
+			private final InitRange lowerNeighbor;
+
+			Range(double[] range, Constants.InitRange lowerNeighbor, Constants.InitRange higherNeighbor)
+			{
+				this.range = range;
+				this.higherNeighbor = higherNeighbor;
+				this.lowerNeighbor = lowerNeighbor;
+			}
+
+			public double[] range()
+			{
+				return this.range;
+			}
+
+			public double bottomValue()
+			{
+				return this.range[0];
+			}
+
+			public double topValue()
+			{
+				return this.range[1];
+			}
+
+			public InitRange lowerNeighbor()
+			{
+				return this.lowerNeighbor;
+			}
+
+			public InitRange higherNeighbor()
+			{
+				return this.higherNeighbor;
+			}
+		}
+
 		//TODO:change numbers to appropriate values
-		public static final int SPIT_CUBE_OUT = 1;
-		public static final int SUCK_CUBE_IN = 500;
-		
+		public static final int AUTO_EJECT_ENCODER_STOP_VALUE = 1;
+		public static final int AUTO_INTAKE_ENCODER_STOP_VALUE = 500;
+
 		public static final int ENCODER_FLOOR_POSITION = 0;
 		public static final int ENCODER_MIDDLE_POSITION = 3000;
 		public static final int ENCODER_RAISED_POSITION = 6000;
-		
+
 		public static final int LEFT_INTAKE_MOTOR_PORT = 0;
 		public static final int RIGHT_INTAKE_MOTOR_PORT = 1;
-		public static final int PIVOT_MOTOR_PORT = 1;
-		
+		public static final int pivoter_MOTOR_PORT = 1;
+
 		public static final int PID_SLOT_ID = 0;
 	}
 
