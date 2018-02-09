aa2662693a7d6bf7bec011c8c1c2f18dddaf331c
MarkShark99 <markdw88@gmail.com>
2018-02-09 15:23:16 -0500

    Made class abstract.

--- src/org/usfirst/frc/team4237/robot/control/Xbox.java
+++ src/org/usfirst/frc/team4237/robot/control/Xbox.java
@@ -9,24 +9,13 @@ import java.util.HashMap;
  * @version 1.0
  *
  */
-public class Xbox extends Joystick
+public abstract class Xbox extends Joystick
 {
-	private static Xbox instance = new Xbox(Constants.PORT);
-
-	/**
-	 * Get instance of Xbox class
-	 * @return
-	 */
-	public static Xbox getInstance()
-	{
-		return instance;
-	}
-
 	/**
 	 * Xbox constructor
 	 * @param port
 	 */
-	private Xbox(int port)
+	protected Xbox(int port)
 	{
 		super(port);
 	}
