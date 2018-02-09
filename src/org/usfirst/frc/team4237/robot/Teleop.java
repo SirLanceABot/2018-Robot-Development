ed3cbb3e3ab1a6fe5344f8d37cffa51cacee5c7e
MarkShark99 <markdw88@gmail.com>
2018-02-09 14:06:30 -0500

    Initial commit

---
+++ src/org/usfirst/frc/team4237/robot/Teleop.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4237.robot;
+
+import org.usfirst.frc.team4237.robot.components.Drivetrain;
+import org.usfirst.frc.team4237.robot.components.Elevator;
+import org.usfirst.frc.team4237.robot.components.Gripper;
+import org.usfirst.frc.team4237.robot.control.Xbox;
+
+public class Teleop
+{
+	private Drivetrain drivetrain = Drivetrain.getInstance();
+	private Elevator elevator = Elevator.getInstance();
+	private Gripper gripper = Gripper.getInstance();
+	private Xbox xbox = Xbox.getInstance();
+	
+	private boolean aButton = false;
+	private boolean bButton = false;
+	private boolean xButton = false;
+	private boolean yButton = false;
+	
+	private static Teleop instance = new Teleop();
+	public static Teleop getInstance()
+	{
+		return instance;
+	}
+	
+	/**
+	 * Teleop init method
+	 */
+	public void init()
+	{
+		
+	}
+	
+	/**
+	 * Teleop loop
+	 */
+	public void periodic()
+	{
+		
+	}
+}
