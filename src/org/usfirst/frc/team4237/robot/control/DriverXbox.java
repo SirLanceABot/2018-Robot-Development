1bd1b1d81b62ca78d0de1315e9d75f6d1ea753d6
MarkShark99 <markdw88@gmail.com>
2018-02-09 15:24:02 -0500

    Initial commit.

---
+++ src/org/usfirst/frc/team4237/robot/control/DriverXbox.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team4237.robot.control;
+
+public class DriverXbox extends Xbox
+{
+	private static DriverXbox instance = new DriverXbox(Constants.PORT);
+	public static DriverXbox getInstance()
+	{
+		return instance;
+	}
+	
+	private DriverXbox(int port)
+	{
+		super(port);
+	}
+	
+	public static class Constants
+	{
+		public static int PORT = 0;
+	}
+}
