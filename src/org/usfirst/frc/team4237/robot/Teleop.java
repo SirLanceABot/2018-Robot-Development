8419c28b1a5170167ca4c865054bdf54f8d903b0
MarkShark99 <markdw88@gmail.com>
2018-02-10 23:30:38 -0500

    Added try-catch block around code in periodic() method.

--- src/org/usfirst/frc/team4237/robot/Teleop.java
+++ src/org/usfirst/frc/team4237/robot/Teleop.java
@@ -12,41 +12,47 @@ public class Teleop
 	private Elevator elevator = Elevator.getInstance();
 	private Gripper gripper = Gripper.getInstance();
 	private DriverXbox xbox = DriverXbox.getInstance();
-	
+
 	private boolean aButton = false;
 	private boolean bButton = false;
 	private boolean xButton = false;
 	private boolean yButton = false;
-	
+
 	private double rightXAxis = 0.0;
 	private double rightYAxis = 0.0;
 	private double leftXAxis = 0.0;
 	private double leftYAxis = 0.0;
-	
+
 	private static Teleop instance = new Teleop();
 	public static Teleop getInstance()
 	{
 		return instance;
 	}
-	
+
 	/**
 	 * Teleop init method
 	 */
 	public void init()
 	{
-		
+
 	}
-	
+
 	/**
 	 * Teleop loop
 	 */
 	public void periodic()
 	{
-		leftXAxis = xbox.getRawAxis(Xbox.Constants.LEFT_STICK_X_AXIS);
-		leftYAxis = xbox.getRawAxis(Xbox.Constants.LEFT_STICK_Y_AXIS);
-		rightXAxis = xbox.getRawAxis(Xbox.Constants.RIGHT_STICK_X_AXIS);
-		
-		drivetrain.driveCartesian(leftXAxis, leftYAxis, rightXAxis);
-		
+		try
+		{
+			leftXAxis = xbox.getRawAxis(Xbox.Constants.LEFT_STICK_X_AXIS);
+			leftYAxis = xbox.getRawAxis(Xbox.Constants.LEFT_STICK_Y_AXIS);
+			rightXAxis = xbox.getRawAxis(Xbox.Constants.RIGHT_STICK_X_AXIS);
+
+			drivetrain.driveCartesian(leftXAxis, leftYAxis, rightXAxis);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
