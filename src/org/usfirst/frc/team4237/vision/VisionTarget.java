fe59149facb6f7822d352132dc121ba2c966dbc6
MarkShark99 <markdw88@gmail.com>
2018-02-10 23:34:07 -0500

    Initial commit.

---
+++ src/org/usfirst/frc/team4237/vision/VisionTarget.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team4237.vision;
+
+public class VisionTarget
+{
+	int height;
+	int midpointX;
+	
+	public int getHeight()
+	{
+		return this.height;
+	}
+	
+	public int getMidpointX()
+	{
+		return this.midpointX;
+	}
+}
